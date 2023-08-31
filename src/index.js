d9bc054c42925cd0eec4158694a1cef85598efae
ncharlemagne <125274331+ncharlemagne@users.noreply.github.com>
2023-08-31 17:03:07 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./componants/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// ReactDOM.render(<h1 className="heading" style={customStyle}>{greeting}</h1>, document.getElementById("root");
